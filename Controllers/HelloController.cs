05ac027b7da6569b47a9115a0d5fa3e552e49c9c
Wandumuzi Mathunjwa <Wandumuzi.Mathunjwa@platinumlife.co.za>
2020-02-29 14:02:33 +0200

    added customers, orders and server controllers

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Advantage.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Hello...", "Dickhead" };
+        }
+    }
+}
---
+++ Controllers/ServersController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Advantage.API.Models;
+
+namespace Advantage.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServersController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ServersController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Servers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Server>>> GetServers()
+        {
+            return await _context.Servers.ToListAsync();
+        }
+
+        // GET: api/Servers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Server>> GetServer(int id)
+        {
+            var server = await _context.Servers.FindAsync(id);
+
+            if (server == null)
+            {
+                return NotFound();
+            }
+
+            return server;
+        }
+
+        // PUT: api/Servers/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutServer(int id, Server server)
+        {
+            if (id != server.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(server).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ServerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Servers
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Server>> PostServer(Server server)
+        {
+            _context.Servers.Add(server);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetServer", new { id = server.Id }, server);
+        }
+
+        // DELETE: api/Servers/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Server>> DeleteServer(int id)
+        {
+            var server = await _context.Servers.FindAsync(id);
+            if (server == null)
+            {
+                return NotFound();
+            }
+
+            _context.Servers.Remove(server);
+            await _context.SaveChangesAsync();
+
+            return server;
+        }
+
+        private bool ServerExists(int id)
+        {
+            return _context.Servers.Any(e => e.Id == id);
+        }
+    }
+}
